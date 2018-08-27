c74f542a09a065c7bd3b8c90ad75e6cdbe87ccaa
Harper Bellows <harper.bellows@gmail.com>
2018-08-27 15:38:10 -0600

    fix test to add route helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   root 'songs#index'
-  resources :songs, only: [:index]
 
   resources :artists, only: [:new, :create, :index], shallow: true do
-    resources :songs
+    resources :songs, only: [:show, :new, :create]
   end
   resources :playlists, only: [:new, :index, :create]
+  resources :songs, only: [:index]
   # resources :playlists
   # resources :artists
 end
